ecb88059b4645ca61fafe30984968e88c80a602d
EC2 Default User <ec2-user@ip-172-31-1-26.ap-northeast-1.compute.internal>
2022-07-07 22:27:34 +0000

    [add]フォロー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,31 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  
+  has_many :relationship, class_name:"Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_relationship, class_name:"Relationship", foreign_key: "followed_id", dependent: :destroy
+  
+  has_many :followings, through: :relationship, source: :followed
+  has_many :followers, through: :reverse_relationship, source: :follower
+  
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: {maximum: 50 }
-
   
+  
+  def follow(user_id)
+    relationship.create(followed_id: user_id)
+  end
+  
+  def unfollow(user_id)
+    relationship.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followers.include?(user)
+  end
+    
   
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
--- db/migrate/20220707082344_create_relationships.rb
+++ db/migrate/20220707082344_create_relationships.rb
@@ -3,6 +3,8 @@ class CreateRelationships < ActiveRecord::Migration[6.1]
     create_table :relationships do |t|
 
       t.timestamps
+      t.integer :follower_id
+      t.integer :followed_id
     end
   end
 end
